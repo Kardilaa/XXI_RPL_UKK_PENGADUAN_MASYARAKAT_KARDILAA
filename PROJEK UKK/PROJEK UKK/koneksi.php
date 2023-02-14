a157fedf1503068f80e83911a4f5ebcbe21f7823
Kardilaa <110007841+Kardilaa@users.noreply.github.com>
2023-02-14 11:09:31 +0700

    Add files via upload

---
+++ PROJEK UKK/PROJEK UKK/koneksi.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$database = "pengaduan_masyarakat";
+$username = "root";
+$password = "";
+ 
+// untuk tulisan bercetak tebal silakan sesuaikan dengan detail database Anda
+// membuat koneksi
+$conn = mysqli_connect($servername, $username, $password, $database);
+// mengecek koneksi
+?>
